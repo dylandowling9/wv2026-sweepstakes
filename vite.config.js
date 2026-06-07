908fae6e7221360c69374604f7272c46e7e46ce1
dylandowling9 <dylandowling9@gmail.com>
2026-06-07 11:28:06 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    target: 'es2015',
+    charset: 'utf8',
+  },
+  server: {
+    fs: {
+      strict: false
+    }
+  }
+})
